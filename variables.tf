a8cec86515c0b9b722532180fa04745551b144a9
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-06-02 13:47:05 +0200

    fix: fix error "attribute from null" and other changes

--- variables.tf
+++ variables.tf
@@ -69,27 +69,29 @@ variable "dependency_ids" {
 #######################
 
 variable "enable_service_monitor" {
-  description = "Enable the deployment of a service monitor."
+  description = "Boolean to enable the deployment of a service monitor."
   type        = bool
   default     = false
 }
 
 variable "enable_dashboard_ingress" {
-  description = "Enable the creation of an ingress for the Longhorn's dashboard."
+  description = "Boolean to enable the creation of an ingress for the Longhorn's dashboard. **If enabled, you must provide a value for `base_domain`.**"
   type        = bool
   default     = false
 }
 
 variable "oidc" {
-  description = "OIDC settings configure Oauth2-Proxy which will be use to protect Longhorn's dashboard."
+  description = "OIDC settings to configure OAuth2-Proxy which will be used to protect Longhorn's dashboard."
+
   type = object({
-    enabled                 = bool
     issuer_url              = string
-    redirect_url            = string
+    oauth_url               = string
+    token_url               = string
+    api_url                 = string
     client_id               = string
     client_secret           = string
-    cookie_secret           = optional(string, "")
-    oauth2_proxy_extra_args = optional(list(string))
+    oauth2_proxy_extra_args = list(string)
   })
-  default = null
+
+  default = {}
 }
