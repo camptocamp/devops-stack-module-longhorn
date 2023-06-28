99d1afb01f6a2800c7255541cacaac90c55a98bf
Hugo Bollon <hugo.bollon@camptocamp.com>
2023-06-28 09:26:32 +0200

    feat: initial module implementation (#1)
    Co-authored-by: hbollon <hbollon@users.noreply.github.com>
    Co-authored-by: lentidas <lentidas@users.noreply.github.com>
    
    Release-As: v1.0.0

--- locals.tf
+++ locals.tf
@@ -1,3 +1,61 @@
 locals {
-  helm_values = [{}]
+  domain      = format("longhorn.apps.%s", var.base_domain)
+  domain_full = format("longhorn.apps.%s.%s", var.cluster_name, var.base_domain)
+
+  helm_values = [{
+    longhorn = {
+      defaultSettings = {
+        backupTarget                      = var.enable_pv_backups ? format("s3://%s@%s/", var.backup_storage.bucket_name, var.backup_storage.region) : ""
+        backupTargetCredentialSecret      = var.enable_pv_backups ? "longhorn-s3-secret" : ""
+        defaultLonghornStaticStorageClass = var.enable_pv_backups && var.set_default_storage_class ? "longhorn-backup" : "longhorn-static"
+        storageOverProvisioningPercentage = var.storage_over_provisioning_percentage
+        storageMinimalAvailablePercentage = var.storage_minimal_available_percentage
+      }
+      persistence = {
+        defaultClass = var.enable_pv_backups && var.set_default_storage_class ? "false" : "true"
+      }
+    }
+    backups = merge({
+      enabled = var.enable_pv_backups
+      }, var.enable_pv_backups ? {
+      defaultStorageClass = var.set_default_storage_class
+      config              = var.backup_configuration
+      storage             = var.backup_storage
+    } : null)
+    oidc = var.oidc != null ? {
+      oauth2_proxy_image      = "quay.io/oauth2-proxy/oauth2-proxy:v7.4.0"
+      issuer_url              = var.oidc.issuer_url
+      redirect_url            = format("https://%s/oauth2/callback", local.domain_full)
+      client_id               = var.oidc.client_id
+      client_secret           = var.oidc.client_secret
+      cookie_secret           = resource.random_string.oauth2_cookie_secret.result
+      oauth2_proxy_extra_args = var.oidc.oauth2_proxy_extra_args
+    } : null
+    ingress = {
+      enabled = var.enable_dashboard_ingress
+      hosts = [
+        local.domain,
+        local.domain_full
+      ]
+      annotations = {
+        "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+        "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+        "traefik.ingress.kubernetes.io/router.tls"         = "true"
+        "ingress.kubernetes.io/ssl-redirect"               = "true"
+        "kubernetes.io/ingress.allow-http"                 = "false"
+      }
+    }
+    grafana_dashboard = {
+      enabled = var.enable_monitoring_dashboard
+    }
+    servicemonitor = {
+      enabled = var.enable_service_monitor
+    }
+  }]
+}
+
+resource "random_string" "oauth2_cookie_secret" {
+  length  = 32
+  special = false
 }
--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "target_revision" {
 variable "namespace" {
   description = "Namespace where the applications's Kubernetes resources should be created. Namespace will be created in case it doesn't exist."
   type        = string
-  default     = "<NAMESPACE>"
+  default     = "longhorn-system"
 }
 
 variable "helm_values" {
@@ -67,3 +67,95 @@ variable "dependency_ids" {
 #######################
 ## Module variables
 #######################
+
+variable "storage_over_provisioning_percentage" {
+  description = "Set the storage over-provisioning percentage. **This values should be modified only when really needed.** The default is 200%, as https://longhorn.io/docs/1.3.1/best-practices/#minimal-available-storage-and-over-provisioning[recommended in the best practices] for single-disk nodes."
+  type        = number
+  default     = 200
+}
+
+variable "storage_minimal_available_percentage" {
+  description = "Set the minimal available storage percentage. **This values should be modified only when really needed.** The default is 25%, as https://longhorn.io/docs/1.3.1/best-practices/#minimal-available-storage-and-over-provisioning[recommended in the best practices] for single-disk nodes."
+  type        = number
+  default     = 25
+}
+
+variable "enable_pv_backups" {
+  description = "Boolean to enable backups of Longhorn volumes to an external object storage."
+  type        = bool
+  default     = false
+}
+
+variable "set_default_storage_class" {
+  description = "Boolean to set the Storage Class with the backup configuration as the default for all Persistent Volumes."
+  type        = bool
+  default     = true
+}
+
+variable "backup_storage" {
+  description = "Exoscale SOS bucket configuration where the backups will be stored. **This configuration is required if the variable `enable_pv_backups` is set to `true`.**"
+  type = object({
+    bucket_name = string
+    region      = string
+    endpoint    = string
+    access_key  = string
+    secret_key  = string
+  })
+  default = null
+}
+
+variable "backup_configuration" {
+  description = <<-EOT
+    The following values can be configured:
+    . `snapshot_cron` - Cron schedule to configure Longhorn automatic snapshots.
+    . `snapshot_retention` - Retention of Longhorn automatic snapshots in days.
+    . `backup_cron` - Cron schedule to configure Longhorn automatic backups.
+    . `backup_retention` - Retention of Longhorn automatic backups in days.
+  EOT
+
+  type = object({
+    snapshot_cron      = string
+    snapshot_retention = number
+    backup_cron        = string
+    backup_retention   = number
+  })
+
+  default = {
+    snapshot_cron      = "0 */2 * * *"
+    snapshot_retention = "1"
+    backup_cron        = "30 */12 * * *"
+    backup_retention   = "2"
+  }
+}
+
+variable "enable_service_monitor" {
+  description = "Boolean to enable the deployment of a service monitor."
+  type        = bool
+  default     = false
+}
+
+variable "enable_dashboard_ingress" {
+  description = "Boolean to enable the creation of an ingress for the Longhorn's dashboard. **If enabled, you must provide a value for `base_domain`.**"
+  type        = bool
+  default     = false
+}
+
+variable "enable_monitoring_dashboard" {
+  description = "Boolean to enable the provisioning of a Longhorn dashboard for Grafana."
+  type        = bool
+  default     = true
+}
+
+variable "oidc" {
+  description = "OIDC settings to configure OAuth2-Proxy which will be used to protect Longhorn's dashboard."
+  type = object({
+    issuer_url              = string
+    oauth_url               = optional(string, "")
+    token_url               = optional(string, "")
+    api_url                 = optional(string, "")
+    client_id               = string
+    client_secret           = string
+    oauth2_proxy_extra_args = optional(list(string), [])
+  })
+  default = null
+}
