265f78321a2e945ec2d032050a5653ec6c5e81cc
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-01-12 15:48:55 +0100

    fix!: remove the namespace variable
    We found out that on multiple modules we were referencing resources from other modules using their default namespaces and this was hardcoded. In case someone decided to change the namespace of deployment of a certain application, this could break the way modules work with each other.
    
    We've decided that in order to avoid undefined behaviors caused by overloading this variable, it would be best to remove it entirely.

--- variables.tf
+++ variables.tf
@@ -50,12 +50,6 @@ variable "target_revision" {
   default     = "v2.3.0" # x-release-please-version
 }
 
-variable "namespace" {
-  description = "Namespace where the applications's Kubernetes resources should be created. Namespace will be created in case it doesn't exist."
-  type        = string
-  default     = "longhorn-system"
-}
-
 variable "helm_values" {
   description = "Helm chart value overrides. They should be passed as a list of HCL structures."
   type        = any
